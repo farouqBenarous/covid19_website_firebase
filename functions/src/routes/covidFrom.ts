8b5b3515a08a1566a60c4a269de1353333727630
farouqbenarous <5hrvlvrurk45>
2020-06-04 12:19:10 +0100

    -api design - schema design - multi language support

--- functions/src/routes/covidFrom.ts
+++ functions/src/routes/covidFrom.ts
@@ -1,33 +1,65 @@
-import {covidForm} from "../models/forms/covidForm";
+import {Covid19PsycschemaEn, Covid19PsycschemaAr, Covid19PsycschemaFr} from "../models/forms/covidForm";
+import {languageSchema} from "../models/language/language";
 import * as firebaseHelper from 'firebase-functions-helper/dist';
 import * as admin from "firebase-admin";
-import * as express  from "express"
-import * as functions from "firebase-functions";
-admin.initializeApp(functions.config().firebase);
+import * as express from "express";
+import * as Joi from "joi";
+import * as _ from "lodash";
+import * as Strings from "../tools/strings";
+
+const translate = require("translate-google");
+
+admin.initializeApp();
 const db = admin.firestore();
-const covidFormCollection = 'contacts';
+const collectionPath = 'forms';
+const documentPath = 'covid19';
+const documentSubPath = 'psychology';
 export const router = express.Router();
 
-router.post('/covid19', async (req, res) => {
-    try {
-        const form: covidForm = {
-            firstName: req.body['firstName'],
-            lastName: req.body['lastName'],
-            email: req.body['email']
+router.post('/covid19/psyc', async (req, res) => {
+    const languageValidation = Joi.validate(req.header('language'), languageSchema);
+    if (!_.isEmpty(languageValidation.error)) {
+        res.status(400).send(languageValidation.error);
+        return;
+    }
+    let formValidation;
+    switch (languageValidation.value) {
+        case 'en':
+            formValidation = Joi.validate(req.body.form, Covid19PsycschemaEn);
+            break;
+        case 'ar':
+            formValidation = Joi.validate(req.body.form, Covid19PsycschemaAr);
+            break;
+        case 'fr':
+            formValidation = Joi.validate(req.body.form, Covid19PsycschemaFr);
+            break;
+        default :
+            formValidation = Joi.validate(req.body.form, Covid19PsycschemaFr);
+    }
+    if (!_.isEmpty(formValidation.error)) {
+        res.status(400).send(formValidation.error)
+        return;
+    } else {
+        try { // todo :  clean the code
+            var reponse = await translate(Strings.responseSentForm, {
+                from: 'en',
+                to: languageValidation.value
+            });
+        }
+        catch (e) {
+            console.log(e);
         }
 
-        const newDoc = await firebaseHelper.firestore
-            .createNewDocument(db, covidFormCollection, form);
-        res.status(201).send(`Created a new contact: ${newDoc.id}`);
-    } catch (error) {
-        res.status(400).send(`Contact should only contains firstName, lastName and email!!!`)
+        db.collection(collectionPath).doc(documentPath).collection(documentSubPath).add(req.body)
+            .then(() => res.status(200).send(reponse))
+            .catch((err) => res.status(400).send(err));
     }
 })
 
 // View all contacts
-router.get('/covid19', (req, res) => {
+router.get('/covid19/psyc', (req, res) => {
     firebaseHelper.firestore
-        .backup(db, covidFormCollection)
+        .backup(db, collectionPath)
         .then(data => res.status(200).send(data))
         .catch(error => res.status(400).send(`Cannot get contacts: ${error}`));
 })
