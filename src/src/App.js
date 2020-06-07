95e531ea371dcfdf03f1331503376666d72f36e7
farouqbenarous <5hrvlvrurk45>
2020-06-07 16:47:55 +0100

    fist commit of front end
    *  template
    * multi language setup

---
+++ src/src/App.js
@@ -0,0 +1,48 @@
+import React, {Fragment, Suspense, lazy} from "react";
+import {MuiThemeProvider, CssBaseline} from "@material-ui/core";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
+import theme from "./theme";
+import GlobalStyles from "./GlobalStyles";
+import * as serviceWorker from "./serviceWorker";
+import Pace from "./shared/components/Pace";
+
+import {IntlProvider, addLocaleData} from "react-intl";
+import en from "react-intl/locale-data/en";
+import messages from './translations/index';
+import flattenMessages from "../src/shared/functions/flattenMessages";
+
+addLocaleData([...en]);
+const locale = "en-US" ;
+
+const LoggedInComponent = lazy(() => import("./logged_in/components/Main"));
+
+const LoggedOutComponent = lazy(() => import("./logged_out/components/Main"));
+
+
+function App() {
+    return (
+        <BrowserRouter>
+            <IntlProvider locale={locale} messages={flattenMessages(messages[locale])}>
+                <MuiThemeProvider theme={theme}>
+                    <CssBaseline/>
+                    <GlobalStyles/>
+                    <Pace color={theme.palette.primary.light}/>
+                    <Suspense fallback={<Fragment/>}>
+                        <Switch>
+                            <Route path="/c">
+                                <LoggedInComponent/>
+                            </Route>
+                            <Route>
+                                <LoggedOutComponent/>
+                            </Route>
+                        </Switch>
+                    </Suspense>
+                </MuiThemeProvider>
+            </IntlProvider>
+        </BrowserRouter>
+    );
+}
+
+serviceWorker.register();
+
+export default App;
---
+++ src/src/translations/en-US.js
@@ -0,0 +1,69 @@
+export default {
+    "en-US": {
+        app: {
+            title: "20x"
+        },
+        navbar: {
+            item_1: "Opportunity Risk Listing",
+            myprofile: "My Profile",
+            logout: "Log Out",
+            brand: {
+                alt: "20x logo"
+            },
+            manageaccount: "Manage Account",
+
+        },
+        footer: {
+            action: "Action",
+            tenants: "Tenants",
+            close: "Close",
+            ignore: "Ignore",
+            account: "Account",
+            users: "Users"
+        },
+        pages: {
+            home: {
+                title: "First Version of covid 19 website using Material-UI"
+            }
+        },
+
+        errors: {
+            unexpected: "Unexpected error, try later.",
+            no_data: {
+                details: "There is no data available for this opportunity.",
+                tenant: "There is no data available for this tenant.",
+                alt: "No data"
+            }
+        },
+        upload: {
+            button: "Upload",
+            uploading: "Uploading",
+            error: "Unexpected error importing the file, try again.",
+            success: "CSV file imported with success.",
+            alt: "Upload button"
+        },
+        action: {
+            button: "Action"
+        },
+        ignore: {
+            button: "Action"
+        },
+
+        action_all: {
+            button: "Action All"
+        },
+        floating: {
+            alt: "Scroll to top"
+        },
+        notification: {
+            alt: "Action triggered"
+        },
+        variance: {
+            alt: "Variance of {value}"
+        },
+        back: {
+            button: "Back",
+            alt: "Go back to previous page"
+        },
+    }
+};
